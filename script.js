42635df8ec45665efbaeba2188ceb03f0fd4816e
Lukas <44616191+lukassh@users.noreply.github.com>
2019-03-20 13:26:50 +0100

    Fix errors

--- script.js
+++ script.js
@@ -1,22 +1,3 @@
-/* var element = React.createElement('div', {}, 'Hello world!'); */
-
-/*
-var element = 
-	React.createElement('div', {},
-		React.createElement('h1', {}, 'Lista filmow'),
-		React.createElement('ul', {},
-			React.createElement('li', {},
-				React.createElement('h2', {}, 'Harry Potter'),
-				React.createElement('p', {}, 'Film o czarodziju')
-			),
-			React.createElement('li', {},
-				React.createElement('h2', {}, 'Krol Lew'),
-				React.createElement('p', {}, 'Film o zwierzetach')
-			)
-		)
-
-	);
-*/
 var movies = [
   {
     id: 1,
